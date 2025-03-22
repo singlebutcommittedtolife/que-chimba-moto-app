0f769d5efd3ab7cfc1e1b22233a96887cd5b9611
Michelle Caicedo <michelleleonor.caicedo@bbva.com>
2025-03-22 11:15:27 +0100

    fix ticjket

--- server/routes/ticketPurchase.js
+++ server/routes/ticketPurchase.js
@@ -56,7 +56,7 @@ router.post('/ticket/generate-ticket', async (req, res) => {
     const timestamp = Date.now();
 
     console.log("concat s "+req.body.clientId);
-    let ticket=timestamp.concat(req.body.clientId);
+    let ticket = timestamp.toString() + req.body.clientId.toString();
     console.log("ticket ",ticket)
 
     // Crear y guardar la compra de ticket
