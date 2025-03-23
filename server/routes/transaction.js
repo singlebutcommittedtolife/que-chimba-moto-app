3d31e69ee8e4bbf0c7cf1420cae43928deae1d2e
Michelle Caicedo <michelleleonor.caicedo@bbva.com>
2025-03-23 16:50:32 +0100

    Elimino fichero .env

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
 const Transaction = require('../models/Transaction');
+require('dotenv').config();
 const axios = require('axios'); // Para Node.js
 
 // Obtener todas las transacciones
